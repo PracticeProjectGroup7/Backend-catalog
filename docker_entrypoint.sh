ba1d3b0fa2a551fbce421881e035ad9fcd67d41d
antimatter96 <me.arpitjain09@gmail.com>
2023-10-04 01:54:42 +0800

    ci: enable local building

---
+++ docker_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="\
+ -Ddemo-host=${POSTGRES_USER} \
+" &
+
+while true; do
+  echo "Buildibng"
+  watch -d -t -g "ls -lR ./src | md5sum" && ./mvnw compile
+  echo "Change detected"
+done
+aa
