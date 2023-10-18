eb89e9330997156daac8892922bb0b262abbcd89
Sheila Rezkia <sheilarezkia28@gmail.com>
2023-10-18 19:56:14 +0800

    Remove unnecessary dependencies

--- src/test/java/org/teamseven/hms/backend/sample/SampleControllerTest.java
+++ src/test/java/org/teamseven/hms/backend/sample/SampleControllerTest.java
@@ -2,14 +2,11 @@ package org.teamseven.hms.backend.sample;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
