e1548e0b26515ed30a270d462f9ba0f2150d8ff6
Sheila Rezkia <sheilarezkia28@gmail.com>
2023-10-26 00:44:43 +0800

    Add doctor catalog get endpoint

--- src/main/java/org/teamseven/hms/backend/doctor/service/DoctorService.java
+++ src/main/java/org/teamseven/hms/backend/doctor/service/DoctorService.java
@@ -1,23 +1,31 @@
 package org.teamseven.hms.backend.doctor.service;
 
+import org.assertj.core.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.teamseven.hms.backend.doctor.dto.DoctorProfile;
 import org.teamseven.hms.backend.sample.dto.SampleResponse;
 import org.teamseven.hms.backend.shared.exception.SampleCustomFieldUnexpectedException;
+import org.teamseven.hms.backend.user.entity.Doctor;
+import org.teamseven.hms.backend.user.entity.DoctorRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class DoctorService {
-    public SampleResponse getSampleResponse() {
-        return new SampleResponse(1, "GET:: doctor controller");
-    }
+    @Autowired private DoctorRepository repository;
 
-    public SampleResponse getSampleResponseNonCustomException() {
-        throw new IllegalArgumentException("test exception");
-    }
+    public List<DoctorProfile> getDoctorProfiles(List<UUID> doctorIds) {
+        List<Doctor> doctors = Lists.newArrayList(repository.findAllById(doctorIds));
 
-    public SampleResponse getSampleResponseCustomException() throws SampleCustomFieldUnexpectedException {
-        throw new SampleCustomFieldUnexpectedException(
-                "test custom exception",
-                new String[]{"test_error_field"}
-        );
+        return doctors.stream().map(it ->
+                DoctorProfile.builder()
+                        .doctorId(it.getDoctorId())
+                        .specialty(it.getSpeciality())
+                        .yearOfExperience(it.getYearsOfExperience().toString())
+                        .build()
+        ).toList();
     }
 }
