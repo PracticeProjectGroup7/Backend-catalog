43738b93a8e1088fdb5cbd65402334ea0f84a6eb
sheilarezkia <sheilarezkia28@gmail.com>
2023-10-28 08:01:55 +0800

    Allow Lab support staff to update test status & result (#33)
    - Changes on this PR add a new endpoint to allow updates of test result & status.
    - This endpoint is accessible provided that the role of the logged in user is a `LAB_SUPPORT_STAFF`
    - It's expected for the request body to always contain the result & status. null updates are possible for result

---
+++ src/test/java/org/teamseven/hms/backend/booking/controller/LabTestControllerTest.java
@@ -0,0 +1,75 @@
+package org.teamseven.hms.backend.booking.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.teamseven.hms.backend.booking.dto.UpdateLabTestRequest;
+import org.teamseven.hms.backend.booking.entity.TestStatus;
+import org.teamseven.hms.backend.booking.service.LabTestService;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class LabTestControllerTest {
+    @Mock
+    private LabTestService service;
+
+    @InjectMocks
+    private LabTestController controller;
+
+    private MockMvc mockMvc;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc =  MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testUpdateTestStatus_successfulUpdate_assertReturn204() throws Exception{
+        UpdateLabTestRequest request = new UpdateLabTestRequest(TestStatus.PENDING, "test result");
+
+        when(service.updateTestStatus(any(), any(), any())).thenReturn(true);
+
+        mockMvc.perform(
+                patch("/api/v1/tests/07fec0a8-7145-11ee-8684-0242ac130003")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isNoContent());
+
+        verify(service).updateTestStatus(
+                UUID.fromString("07fec0a8-7145-11ee-8684-0242ac130003"), "test result", TestStatus.PENDING
+        );
+    }
+
+    @Test
+    public void testUpdateAppointment_unsuccessfulUpdate_assertReturn304() throws Exception{
+        UpdateLabTestRequest request = new UpdateLabTestRequest(TestStatus.PENDING, "test result");
+
+        when(service.updateTestStatus(any(), any(), any())).thenReturn(false);
+
+        mockMvc.perform(
+                patch("/api/v1/tests/07fec0a8-7145-11ee-8684-0242ac130003")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isNotModified());
+
+        verify(service).updateTestStatus(
+                UUID.fromString("07fec0a8-7145-11ee-8684-0242ac130003"), "test result", TestStatus.PENDING
+        );
+    }
+}
