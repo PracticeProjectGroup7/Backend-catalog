ed636e90c81539c0ee5da9ab0cb8cfb03d50a076
sheilarezkia <sheilarezkia28@gmail.com>
2023-10-28 08:01:55 +0800

    Allow Lab support staff to update test status & result (#33)
    - Changes on this PR add a new endpoint to allow updates of test result & status.
    - This endpoint is accessible provided that the role of the logged in user is a `LAB_SUPPORT_STAFF`
    - It's expected for the request body to always contain the result & status. null updates are possible for result

---
+++ src/main/java/org/teamseven/hms/backend/booking/controller/LabTestController.java
@@ -0,0 +1,35 @@
+package org.teamseven.hms.backend.booking.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.teamseven.hms.backend.booking.annotation.LabTestStatusUpdateAccessValidated;
+import org.teamseven.hms.backend.booking.dto.UpdateLabTestRequest;
+import org.teamseven.hms.backend.booking.service.LabTestService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/tests")
+public class LabTestController {
+    @Autowired private LabTestService labTestService;
+
+//    @LabTestStatusUpdateAccessValidated
+    @PatchMapping("/{testId}")
+    public ResponseEntity updateLabTest(
+            @Valid @RequestBody UpdateLabTestRequest updateLabTestRequest,
+            @PathVariable UUID testId
+    ) {
+        boolean isUpdateSuccesful = labTestService.updateTestStatus(
+                testId,
+                updateLabTestRequest.getResult(),
+                updateLabTestRequest.getStatus()
+        );
+
+        return ResponseEntity
+                .status(isUpdateSuccesful? HttpStatus.NO_CONTENT : HttpStatus.NOT_MODIFIED)
+                .build();
+    }
+}
